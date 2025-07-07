9bc4af5e6aa50f930b541a3e9839ee8854b8d737
megafox3000 <sinkler.work@gmail.com>
2025-07-07 23:54:25 +0200

    Update app.py
    new CORS method DELETE

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ CORS(app, resources={r"/*": {"origins": [
     "https://megafox3000.github.io",
     "http://localhost:5500",
     "http://127.0.0.1:5500"
-], "methods": ["GET", "POST", "OPTIONS", "HEAD"], "allow_headers": ["Content-Type", "Authorization", "X-Requested-With"]}}, supports_credentials=True)
+], "methods": ["GET", "POST", "OPTIONS", "HEAD", "DELETE"], "allow_headers": ["Content-Type", "Authorization", "X-Requested-With"]}}, supports_credentials=True)
 
 
 # Конфигурация Cloudinary
